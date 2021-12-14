eaf01868c90e038312d2d28587be859b2d56c23b
katsumikusumi <kusumi_katsumi@andfactory.co.jp>
2021-12-14 13:36:02 +0900

    initial commit.

---
+++ lib/data/repository/article_repository.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_qiita_retrofit/data/client/api_client.dart';
+import 'package:flutter_qiita_retrofit/data/model/article.dart';
+import 'package:flutter_qiita_retrofit/data/response/result.dart';
+
+abstract class ArticleRepository {
+  Future<Result<List<Article>>> fetchArticles();
+}
+
+class ArticleRepositoryImpl with ArticleRepository {
+  final ApiClient _client;
+
+  ArticleRepositoryImpl([ApiClient? client])
+      : _client = client ?? ApiClient(Dio());
+
+  @override
+  Future<Result<List<Article>>> fetchArticles() {
+    return _client
+        .fetchArticles()
+        .then((articles) => Result<List<Article>>.success(articles))
+        .catchError((error) => Result<List<Article>>.failure(error));
+  }
+}
